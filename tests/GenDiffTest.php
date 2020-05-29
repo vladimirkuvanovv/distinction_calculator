fd56108af8731dfa2f4ebcac3af41b2a3afff3d0
Vladimir <vovan986021@gmail.com>
2020-05-29 23:21:54 +0300

    add trial test

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -1,12 +1,13 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
+namespace App\Tests;
 
+use PHPUnit\Framework\TestCase;
 
 class GenDiffTest extends TestCase
 {
     public function testGenDiff()
     {
-
+        $this->assertEquals(1, 1);
     }
 }
