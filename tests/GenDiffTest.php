4cce37a146451e8988bf9ab8c2df260de02a3dae
Vladimir <vovan986021@gmail.com>
2020-06-03 12:34:58 +0300

    add test method

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -5,6 +5,7 @@ namespace App\Tests;
 use PHPUnit\Framework\TestCase;
 
 use function App\Gendiff\genDiff;
+use function App\Gendiff\getContent;
 
 class GenDiffTest extends TestCase
 {
@@ -12,5 +13,17 @@ class GenDiffTest extends TestCase
     {
         $expected = file_get_contents(__DIR__ . '/fixtures/fixture');
         $this->assertEquals($expected, genDiff(__DIR__ . '/fixtures/before.json', __DIR__ . '/fixtures/after.json'));
+    }
+
+    public function testGetContent()
+    {
+        $expected = file_get_contents(__DIR__ . '/fixtures/before.json');
+        $this->assertEquals($expected, getContent(__DIR__ . '/fixtures/before.json'));
+    }
+
+    public function testGetContent()
+    {
+        $expected = file_get_contents(__DIR__ . '/fixtures/before.json');
+        $this->assertEquals($expected, getContent(__DIR__ . '/fixtures/before.json'));
     }
 }
