015c57dd2ac7574fa7653d6e9f93df39b8f4877f
Vladimir <vovan986021@gmail.com>
2020-06-05 23:03:34 +0300

    add tests

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -6,13 +6,17 @@ use PHPUnit\Framework\TestCase;
 
 use function App\Gendiff\genDiff;
 use function App\Gendiff\getContent;
+use function App\Gendiff\compareArrays;
 
 class GenDiffTest extends TestCase
 {
     public function testGenDiff()
     {
-        $expected = file_get_contents(__DIR__ . '/fixtures/fixture');
+        $expected = file_get_contents(__DIR__.'/fixtures/right');
         $this->assertEquals($expected, genDiff(__DIR__ . '/fixtures/before.json', __DIR__ . '/fixtures/after.json'));
+
+        $wrong_expected = file_get_contents(__DIR__.'/fixtures/wrong');
+        $this->assertNotEquals($wrong_expected, genDiff(__DIR__ . '/fixtures/before.json', __DIR__ . '/fixtures/after.json'));
     }
 
     public function testGetContent()
@@ -21,9 +25,43 @@ class GenDiffTest extends TestCase
         $this->assertEquals($expected, getContent(__DIR__ . '/fixtures/before.json'));
     }
 
-    public function testGetContent()
+    public function testException()
     {
-        $expected = file_get_contents(__DIR__ . '/fixtures/before.json');
-        $this->assertEquals($expected, getContent(__DIR__ . '/fixtures/before.json'));
+        $this->expectException(\Exception::class);
+
+        getContent( '/fixtures/before.json');
+
+    }
+
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testCompareArrays($first, $second, $expected)
+    {
+        $this->assertEquals($expected, compareArrays($first, $second));
+    }
+
+    public function additionProvider()
+    {
+        return [
+            [
+                [
+                    'host' => 'hexlet.io',
+                    'timeout' => 50,
+                    'proxy' => '123.234.53.22'
+                ],
+                [
+                    'timeout' => 20,
+                    'host' => 'hexlet.io',
+                    'verbose' => true
+                ],
+                [
+                    'host' => ['value' => 'hexlet.io'],
+                    'timeout' => ['value' => 20, 'diff' => 50],
+                    'proxy' => ['diff' => '123.234.53.22', 'value' => null],
+                    'verbose' => ['value' => true, 'diff' => null]
+                ],
+            ],
+        ];
     }
 }
