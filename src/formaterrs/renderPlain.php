3381c085790e8d17fda06efb8873cf2949adcb5b
Vladimir <vovan986021@gmail.com>
2020-06-25 20:21:31 +0300

    change renderPlain

--- src/formaterrs/renderPlain.php
+++ src/formaterrs/renderPlain.php
@@ -4,45 +4,7 @@ namespace App\Gendiff\Formatter;
 
 function renderPlain(array $tree)
 {
-    /*$resultForPlain = array_map(function ($item) {
-        if ($item['type']) {
-            switch ($item['type']) {
-                case 'nested':
-                    $newChildren = renderPlain($item['children']);
-//                    return $item['key'] . ':' . $newChildren;
-                    break;
-                case 'changed':
-                    $resultPlain[] = sprintf(
-                        "Property '%s' was changed. From '%s' to '%s'",
-                        $item['key'],
-                        toString($item['currentValue']),
-                        toString($item['previousValue'])
-                    );
-                    break;
-                case 'removed':
-                    $resultPlain[] = sprintf("Property '%s' was removed", $item['key']);
-                    break;
-                case 'added':
-                    $resultPlain[] = sprintf(
-                        "Property '%s' was added with value: '%s'",
-                        $item['key'],
-                        toString($item['currentValue'])
-                    );
-                    break;
-            }
-
-            return implode(PHP_EOL, $resultPlain);
-        }
-    }, $tree);
-
-    $resultForPlain = array_filter($resultForPlain, function ($property) {
-        return !is_null($property);
-    });
-
-    return implode(PHP_EOL, $resultForPlain);*/
-
     return buildPlain($tree, '');
-
 }
 
 function buildPlain($tree, $acc)
