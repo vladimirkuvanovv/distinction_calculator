ca8677b1cdfa025b70ec7cf0005c22bcd9268db1
Vladimir <vovan986021@gmail.com>
2020-07-11 12:49:40 +0300

    delete isset conditions

--- src/Builder.php
+++ src/Builder.php
@@ -9,41 +9,31 @@ function builderTree($dataBefore, $dataAfter)
     );
 
     return array_map(function ($key) use ($dataBefore, $dataAfter) {
-        if (
-            isset($dataBefore[$key], $dataAfter[$key])
-            && is_array($dataBefore[$key])
-            && is_array($dataAfter[$key])
-        ) {
-            return getValueForNode($key, 'nested', $dataBefore[$key], $dataAfter[$key]);
+        if (is_array($dataBefore[$key]) && is_array($dataAfter[$key])) {
+            return buildNode($key, 'nested', $dataBefore[$key], $dataAfter[$key]);
         }
 
         if (!isset($dataBefore[$key]) && isset($dataAfter[$key])) {
-            return getValueForNode($key, 'added', [], $dataAfter[$key]);
+            return buildNode($key, 'added', [], $dataAfter[$key]);
         }
 
         if (isset($dataBefore[$key]) && !isset($dataAfter[$key])) {
-            return getValueForNode($key, 'removed', $dataBefore[$key]);
+            return buildNode($key, 'removed', $dataBefore[$key]);
         }
 
-        if (
-            isset($dataBefore[$key], $dataAfter[$key])
-            && ($dataBefore[$key] === $dataAfter[$key])
-        ) {
-            return getValueForNode($key, 'unchanged', $dataBefore[$key]);
+        if ($dataBefore[$key] === $dataAfter[$key]) {
+            return buildNode($key, 'unchanged', $dataBefore[$key]);
         }
 
-        if (
-            isset($dataBefore[$key], $dataAfter[$key])
-            && ($dataBefore[$key] !== $dataAfter[$key])
-        ) {
-            return getValueForNode($key, 'changed', $dataBefore[$key], $dataAfter[$key]);
+        if ($dataBefore[$key] !== $dataAfter[$key]) {
+            return buildNode($key, 'changed', $dataBefore[$key], $dataAfter[$key]);
         }
 
         return [];
     }, $unique_keys);
 }
 
-function getValueForNode($key, $type, $dataBefore = [], $dataAfter = [])
+function buildNode($key, $type, $dataBefore = [], $dataAfter = [])
 {
     switch ($type) {
         case 'nested':
@@ -78,4 +68,6 @@ function getValueForNode($key, $type, $dataBefore = [], $dataAfter = [])
                 'currentValue' => $dataBefore,
             ];
     }
+
+    return [];
 }
