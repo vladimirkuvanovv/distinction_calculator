99b87264f88d779de8290028c32c02c810c8ced7
Vladimir <vovan986021@gmail.com>
2020-07-12 22:34:58 +0300

    change buildNode

--- src/Builder.php
+++ src/Builder.php
@@ -9,8 +9,11 @@ function builderTree($dataBefore, $dataAfter)
     );
 
     return array_map(function ($key) use ($dataBefore, $dataAfter) {
-        if (is_array($dataBefore[$key]) && is_array($dataAfter[$key])) {
-            return buildNode($key, 'nested', $dataBefore[$key], $dataAfter[$key]);
+        if (
+            isset($dataBefore[$key], $dataAfter[$key])
+            && is_array($dataBefore[$key]) && is_array($dataAfter[$key])
+        ) {
+            return buildNode($key, 'nested', [], [], builderTree($dataBefore[$key], $dataAfter[$key]));
         }
 
         if (!isset($dataBefore[$key]) && isset($dataAfter[$key])) {
@@ -22,7 +25,7 @@ function builderTree($dataBefore, $dataAfter)
         }
 
         if ($dataBefore[$key] === $dataAfter[$key]) {
-            return buildNode($key, 'unchanged', $dataBefore[$key]);
+            return buildNode($key, 'unchanged', [], $dataAfter[$key]);
         }
 
         if ($dataBefore[$key] !== $dataAfter[$key]) {
@@ -33,41 +36,13 @@ function builderTree($dataBefore, $dataAfter)
     }, $unique_keys);
 }
 
-function buildNode($key, $type, $dataBefore = [], $dataAfter = [])
+function buildNode($key, $type, $dataBefore = [], $dataAfter = [], $children = [])
 {
-    switch ($type) {
-        case 'nested':
-            return [
-                'key'      => $key,
-                'type'     => $type,
-                'children' => builderTree($dataBefore, $dataAfter)
-            ];
-        case 'added':
-            return [
-                'key'          => $key,
-                'type'         => $type,
-                'currentValue' => $dataAfter,
-            ];
-        case 'removed':
-            return [
-                'key'           => $key,
-                'type'          => $type,
-                'previousValue' => $dataBefore,
-            ];
-        case 'changed':
-            return [
-                'key'           => $key,
-                'type'          => $type,
-                'previousValue' => $dataBefore,
-                'currentValue'  => $dataAfter
-            ];
-        case 'unchanged':
-            return [
-                'key'          => $key,
-                'type'         => $type,
-                'currentValue' => $dataBefore,
-            ];
-    }
-
-    return [];
+    return [
+        'key'           => $key,
+        'type'          => $type,
+        'previousValue' => $dataBefore,
+        'currentValue'  => $dataAfter,
+        'children'      => $children
+    ];
 }
