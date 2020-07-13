b408ecf37c717478c27d533a0998f82fb66cb581
Vladimir <vovan986021@gmail.com>
2020-07-13 23:27:30 +0300

    add variable in builder

--- src/Builder.php
+++ src/Builder.php
@@ -8,36 +8,36 @@ function builderTree($dataBefore, $dataAfter)
         array_unique(array_merge(array_keys($dataBefore), array_keys($dataAfter)))
     );
 
-    return array_map(function ($key) use ($dataBefore, $dataAfter) {
-        if (is_array($dataBefore[$key]) && is_array($dataAfter[$key])) {
-            return buildNode($key, 'nested', null, null, builderTree($dataBefore[$key], $dataAfter[$key]));
-        }
-
-        if (!isset($dataBefore[$key]) && isset($dataAfter[$key])) {
+     $tree = array_map(function ($key) use ($dataBefore, $dataAfter) {
+        if (!isset($dataBefore[$key])) {
             return buildNode($key, 'added', null, $dataAfter[$key]);
         }
 
-        if (isset($dataBefore[$key]) && !isset($dataAfter[$key])) {
+        if (!isset($dataAfter[$key])) {
             return buildNode($key, 'removed', $dataBefore[$key]);
         }
 
-        if ($dataBefore[$key] === $dataAfter[$key]) {
-            return buildNode($key, 'unchanged', null, $dataAfter[$key]);
+        if (is_array($dataBefore[$key]) && is_array($dataAfter[$key])) {
+            return buildNode($key, 'nested', null, null, builderTree($dataBefore[$key], $dataAfter[$key]));
         }
 
         if ($dataBefore[$key] !== $dataAfter[$key]) {
             return buildNode($key, 'changed', $dataBefore[$key], $dataAfter[$key]);
         }
+
+        return buildNode($key, 'unchanged', $dataBefore[$key], $dataAfter[$key]);
     }, $unique_keys);
+
+    return $tree;
 }
 
-function buildNode($key, $type, $dataBefore = [], $dataAfter = [], $children = [])
+function buildNode($key, $type, $dataBefore = null, $dataAfter = null, $children = [])
 {
     return [
         'key'           => $key,
         'type'          => $type,
-        'previousValue' => $dataBefore,
-        'currentValue'  => $dataAfter,
+        'dataBefore'    => $dataBefore,
+        'dataAfter'     => $dataAfter,
         'children'      => $children
     ];
 }
